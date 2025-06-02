b8a82494ac034bf862e0d3eeb3d31089dd76636f
Tabitha-1 <tabbycat78@hotmail.com>
2025-06-02 11:47:07 -0700

    Injected the forecast HTML into JS

--- src/index.js
+++ src/index.js
@@ -60,11 +60,40 @@ function handleSearchSubmit(event) {
 
 
 
+
+function displayForecast() {
+  
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat",];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">⛅</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>70°</strong>
+          </div>
+          <div class="weather-forecast-temperature">50°</div>
+        </div> 
+      </div> 
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Palm Springs");
+displayForecast();
 
 
 //console.log(response.data);
